b7f5c7f4fe4012ed3218acbf43ad2576310572c4
Siti Nuraini <smkiu.012@gmail.com>
2026-04-20 13:28:41 +0700

    categori

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('categori', function () {
+    return view('categori.index');
+});
